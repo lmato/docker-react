11bee733a9042b837e22d4af0bc0e4e7cef3f691
Leonardo Mato <lmato24@gmail.com>
2021-10-11 15:47:21 -0300

    feature branch modified

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed again!
+          Feature branch modified!
         </p>
         <a
           className="App-link"
